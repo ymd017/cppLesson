8df2e42dc2c2e584e407e83207f0f78d0a04abba
ymd017 <yamadareina1027@gmail.com>
2025-01-07 19:55:36 +0900

    Create chapter2_4.cpp

---
+++ yasashi_cpp_practice/chapter2_4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "10進数で表示する\n";
+	cout << 6 << "\n";
+	cout << 20 << "\n";
+	cout << 13 << "\n";
+
+	cout << "\n";
+
+	cout << "8進数で表示する\n";
+	cout << 06 << "\n";
+	cout << 024 << "\n";
+	cout << 015 << "\n";
+
+	cout << "\n";
+
+	cout << "16進数で表示する\n";
+	cout << 0x6 << "\n";
+	cout << 0x14 << "\n";
+	cout << 0xD << "\n";
+}
