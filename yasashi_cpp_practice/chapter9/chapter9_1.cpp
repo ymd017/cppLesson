befc76a315716c2066dcb8ae23cc95b189331c09
ymd017 <yamadareina1027@gmail.com>
2025-01-07 23:25:33 +0900

    Create chapter9_1.cpp

---
+++ yasashi_cpp_practice/chapter9/chapter9_1.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int max(int a[], int n);
+
+int main() {
+
+	int num;
+
+	cout << "生徒の数を入力してください。\n";
+	cin >> num;
+
+	//動的な配列を用意
+	int* test = new int[num];
+
+	//点数を代入する
+	cout << "テストの点数を入力してください。\n";
+
+	for (int i = 0; i < num; i++) {
+		cin >> test[i];
+	}
+
+	//最大値を求める関数の使用と出力
+	cout << "最高点は" << max(test, num) << "です。\n";
+	
+}
+
+int max(int a[], int n) {
+	int max = -10000;
+
+	for (int i = 0; i < n; i++) {
+		if (max < a[i]) {
+			max = a[i];
+		}
+	}
+
+	return max;
+}
