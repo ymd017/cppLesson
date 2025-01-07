10337c29a5d5881c0bb129a53122f117362886e5
ymd017 <yamadareina1027@gmail.com>
2025-01-07 20:52:34 +0900

    Create chapter3_3.cpp

---
+++ yasashi_cpp_practice/chapter3/chapter3_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double height;
+	double weight;
+
+	cout << "身長と体重を入力してください。\n";
+	cin >> height >> weight;
+
+	cout << "身長は" << height << "センチです。\n";
+	cout << "体重は" << weight << "キロです。\n";
+
+}
